1ade8ec5ee570488a50fffd0175138a8634b5b40
Simon Rydberg <simpor81@gmail.com>
2022-12-02 06:49:07 +0100

    day 3 template

---
+++ src/main/kotlin/year2022/Day03.kt
@@ -0,0 +1,26 @@
+import AoCUtils
+import AoCUtils.test
+
+fun main() {
+
+    fun part1(input: String, debug: Boolean = false): Long {
+        return 1L
+
+    }
+
+    fun part2(input: String, debug: Boolean = false): Long {
+
+        return 1L
+
+    }
+
+    val testInput = "xx"
+
+    val input = AoCUtils.readText("year2022/day03.txt")
+
+    part1(testInput) test Pair(0L, "test 1 part 1")
+    part1(input) test Pair(0L, "part 1")
+
+    part2(testInput) test Pair(0L, "test 2 part 2")
+    part2(input) test Pair(0L, "part 2")
+}
