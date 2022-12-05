aee0bac82c3cf4906700f393c7fe11eed65197da
Simon Rydberg <simpor81@gmail.com>
2022-12-05 18:56:11 +0100

    day 5 done

--- src/main/kotlin/year2022/Day05.kt
+++ src/main/kotlin/year2022/Day05.kt
@@ -1,4 +1,3 @@
-import AoCUtils
 import AoCUtils.test
 
 fun main() {
@@ -7,19 +6,71 @@ fun main() {
 
 
     fun part1(input: String, debug: Boolean = false): String {
-        val crates = mutableListOf<Int>()
+        val towers = input.lines().filter { it.startsWith(" 1 ") }.map { it.replace(" ", "") }.last().length
+        val crates = mutableListOf<MutableList<String>>()
 
-        val towers = input.lines().filter { it.startsWith(" 1 ") }.map { it.replace(" ", "") }.last().toInt()
+        for (a in 1..towers) {
+            crates.add(mutableListOf())
+        }
+
 
         input.lines().filter { it.contains("[") }
-            .map {  }
+            .map { line ->
+                line.chunked(4)
+                    .map { it.trim().replace("]", "").replace("[", "") }
+                    .mapIndexed { index, s ->
+                        if (s.isNotBlank()) crates[index].add(0, s)
+//                        println("$index -> $s")
+                    }
+            }
 
+        val craneMovements = input.lines().filter { it.contains("move") }.map { line ->
+            val a = line.split("move|from|to".toRegex()).map { it.trim() }
+            Crane(a[1].toInt(), a[2].toInt(), a[3].toInt())
+        }
 
-        return ""
+        craneMovements.forEach { movement ->
+            for (crate in 1..movement.move) {
+                crates[movement.to - 1].add(crates[movement.from - 1].removeLast())
+            }
+        }
+
+        return crates.map { it.last() }.joinToString("")
     }
 
-    fun part2(input: String, debug: Boolean = false): Long {
-        return 0L
+    fun part2(input: String, debug: Boolean = false): String {
+        val towers = input.lines().filter { it.startsWith(" 1 ") }.map { it.replace(" ", "") }.last().length
+        val crates = mutableListOf<MutableList<String>>()
+
+        for (a in 1..towers) {
+            crates.add(mutableListOf())
+        }
+
+
+        input.lines().filter { it.contains("[") }
+            .map { line ->
+                line.chunked(4)
+                    .map { it.trim().replace("]", "").replace("[", "") }
+                    .mapIndexed { index, s ->
+                        if (s.isNotBlank()) crates[index].add(0, s)
+//                        println("$index -> $s")
+                    }
+            }
+
+        val craneMovements = input.lines().filter { it.contains("move") }.map { line ->
+            val a = line.split("move|from|to".toRegex()).map { it.trim() }
+            Crane(a[1].toInt(), a[2].toInt(), a[3].toInt())
+        }
+
+        craneMovements.forEach { movement ->
+            val list = mutableListOf<String>()
+            for (crate in 1..movement.move) {
+                list.add(0, crates[movement.from - 1].removeLast())
+            }
+            crates[movement.to - 1].addAll(list)
+        }
+
+        return crates.map { it.last() }.joinToString("")
     }
 
     val testInput =
@@ -36,8 +87,8 @@ fun main() {
     val input = AoCUtils.readText("year2022/day05.txt")
 
     part1(testInput, false) test Pair("CMZ", "test 1 part 1")
-    part1(input, false) test Pair("490L", "part 1")
+    part1(input, false) test Pair("SHQWSRBDL", "part 1")
 
-    part2(testInput, false) test Pair(4L, "test 2 part 2")
-    part2(input) test Pair(921L, "part 2")
+    part2(testInput, false) test Pair("MCD", "test 2 part 2")
+    part2(input) test Pair("921L", "part 2")
 }
