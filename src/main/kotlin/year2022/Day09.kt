cf3a115e6e925ecb9d41a489cbbd926cad74907f
Simon Rydberg <simpor81@gmail.com>
2022-12-18 14:06:35 +0100

    day 9 part 2 ongoing

--- src/main/kotlin/year2022/Day09.kt
+++ src/main/kotlin/year2022/Day09.kt
@@ -77,8 +77,51 @@ fun main() {
         return map.values.count { it }.toLong()
     }
 
+    fun moveSnake(snake: Map<Int, Point>, i: Int, move: (Point) -> Point): Pair<Int, Point> {
+        val current = snake[i]!!
+        val pair = if (i == 0) {
+            Pair(0, move.invoke(current))
+        } else {
+            val prev = snake[i - 1]!!
+            val newPrev = move.invoke(prev)
+            if (!newPrev.isAround(current)) {
+                Pair(i, prev)
+            } else {
+                Pair(i, current)
+            }
+        }
+        return pair
+    }
+
     fun part2(input: String, debug: Boolean = false): Long {
-        return 0L
+        val moves = input.lines().map { l ->
+            val s = l.split(" ")
+            Pair(s[0], s[1].toInt())
+        }
+
+        var snake = (0..9).map { Pair(it, Point(0, 0)) }.toMap()
+        val map = mutableMapOf<Point, Boolean>()
+
+        moves.forEach { move ->
+            val updatePos = when (move.first) {
+                "U" -> { pos:Point -> pos.copy(y = pos.y + 1) }
+                "D" -> { pos:Point -> pos.copy(y = pos.y - 1) }
+                "R" -> { pos:Point -> pos.copy(y = pos.x + 1) }
+                "L" -> { pos:Point -> pos.copy(y = pos.x - 1) }
+                else -> throw Exception("Unknown move: $move")
+            }
+
+            repeat(move.second) {
+                snake = snake.keys.sorted().associate { i ->
+                    moveSnake(snake, i) { pos -> updatePos.invoke(pos) }
+                }
+                map[snake[9]!!] = true
+            }
+        }
+
+
+
+        return map.values.count { it }.toLong()
     }
 
     val testInput =
@@ -96,14 +139,16 @@ fun main() {
     part1(testInput, false) test Pair(13L, "test 1 part 1")
     part1(input, false) test Pair(5858L, "part 1")
 
-    part2(testInput, false) test Pair(1L, "test 2 part 2")
-    part2("R 5\n" +
-            "U 8\n" +
-            "L 8\n" +
-            "D 3\n" +
-            "R 17\n" +
-            "D 10\n" +
-            "L 25\n" +
-            "U 20", false) test Pair(36L, "test 3 part 2")
-    part2(input) test Pair(0L, "part 2")
+    part2(testInput, false) test Pair(1L, "test 1 part 2")
+    part2(
+        "R 5\n" +
+                "U 8\n" +
+                "L 8\n" +
+                "D 3\n" +
+                "R 17\n" +
+                "D 10\n" +
+                "L 25\n" +
+                "U 20", false
+    ) test Pair(36L, "test 2 part 2")
+    part2(input) test Pair(2602L, "part 2")
 }
