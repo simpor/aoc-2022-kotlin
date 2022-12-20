459b871d6af67241f38fbc6138473737e921bd1f
Simon Rydberg <simpor81@gmail.com>
2022-12-20 23:20:07 +0100

    day 20 part 2 ongoing

---
+++ src/main/kotlin/year2022/Day21.kt
@@ -0,0 +1,25 @@
+import AoCUtils
+import AoCUtils.test
+
+fun main() {
+
+
+    fun part1(input: String, debug: Boolean = false): Long {
+        return 0L
+    }
+
+    fun part2(input: String, debug: Boolean = false): Long {
+        return 0L
+    }
+
+    val testInput =
+        ""
+
+    val input = AoCUtils.readText("year2022/day21.txt")
+
+    part1(testInput, false) test Pair(24L, "test 1 part 1")
+    part1(input, false) test Pair(0L, "part 1")
+
+    part2(testInput, false) test Pair(0L, "test 2 part 2")
+    part2(input) test Pair(0L, "part 2")
+}
