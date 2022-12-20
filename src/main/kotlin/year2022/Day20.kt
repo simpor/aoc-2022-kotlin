0bf756a1ef77de143442327279cee8d940c636d4
Simon Rydberg <simpor81@gmail.com>
2022-12-20 23:17:45 +0100

    day 20 part 2 ongoing

--- src/main/kotlin/year2022/Day20.kt
+++ src/main/kotlin/year2022/Day20.kt
@@ -4,6 +4,34 @@ fun main() {
     data class LinkedList(val value: Long) {
         lateinit var prev: LinkedList
         lateinit var next: LinkedList
+
+        fun moveBack() {
+            val a = this.prev.prev
+            val b = this.prev
+            val d = this.next
+
+            a.next = this
+            this.next = b
+            b.next = d
+
+            this.prev = a
+            b.prev = this
+            d.prev = b
+        }
+
+        fun moveForward() {
+            val b = this.prev
+            val d = this.next
+            val e = this.next.next
+
+            b.next = d
+            d.next = this
+            this.next = e
+
+            d.prev = b
+            this.prev = d
+            e.prev = this
+        }
     }
 
     fun debugPrintOrder(num1: LinkedList, c: String) {
@@ -17,8 +45,8 @@ fun main() {
         println()
     }
 
-    fun part1(input: String, debug: Boolean = false): Long {
-        val list = input.lines().map { it.toLong() }
+    fun parse(input: String, times: Int = 1): List<LinkedList> {
+        val list = input.lines().map { it.toLong() * times }
             .map { LinkedList(it) }
         for (i in 1 until list.size) {
             val prev = list[i - 1]
@@ -28,49 +56,10 @@ fun main() {
         }
         list.first().prev = list.last()
         list.last().next = list.first()
-        if (debug) debugPrintOrder(list.first(), "initial")
+        return list
+    }
 
-        list.forEach { c ->
-            var value = c.value
-            while (value != 0L) {
-                if (value < 0) {
-                    val a = c.prev.prev
-                    val b = c.prev
-                    val d = c.next
-
-                    a.next = c
-                    c.next = b
-                    b.next = d
-
-                    c.prev = a
-                    b.prev = c
-                    d.prev = b
-
-                    value++
-                } else {
-                    val b = c.prev
-                    val d = c.next
-                    val e = c.next.next
-
-                    b.next = d
-                    d.next = c
-                    c.next = e
-
-                    d.prev = b
-                    c.prev = d
-                    e.prev = c
-
-                    value--
-                }
-            }
-
-            val num1 = list.first()
-            if (debug) debugPrintOrder(num1, c.value.toString())
-
-
-        }
-
-
+    fun getResult(list: List<LinkedList>): MutableList<Long> {
         val num0 = list.filter { it.value == 0L }.first()
 
         var loopis = num0.next
@@ -84,21 +73,37 @@ fun main() {
                 resultList.add(loopis.value)
             }
         }
+        return resultList
+    }
+
+    fun part1(input: String, debug: Boolean = false): Long {
+        val list = parse(input)
+        if (debug) debugPrintOrder(list.first(), "initial")
+
+        list.forEach { c ->
+            var value = c.value
+            while (value != 0L) {
+                if (value < 0) {
+                    c.moveBack()
+                    value++
+                } else {
+                    c.moveForward()
+                    value--
+                }
+            }
+
+            val num1 = list.first()
+            if (debug) debugPrintOrder(num1, c.value.toString())
+        }
+
+        val resultList = getResult(list)
 
         return resultList.sum()
     }
 
     fun part2(input: String, debug: Boolean = false): Long {
-        val list = input.lines().map { it.toLong() * 811589153 }
-            .map { LinkedList(it) }
-        for (i in 1 until list.size) {
-            val prev = list[i - 1]
-            val current = list[i]
-            prev.next = current
-            current.prev = prev
-        }
-        list.first().prev = list.last()
-        list.last().next = list.first()
+        val list = parse(input, 811589153)
+
         if (debug) debugPrintOrder(list.first(), "initial")
 
         repeat(10) { repeatNum ->
@@ -106,57 +111,20 @@ fun main() {
                 var value = c.value % list.size
                 while (value != 0L) {
                     if (value < 0) {
-                        val a = c.prev.prev
-                        val b = c.prev
-                        val d = c.next
-
-                        a.next = c
-                        c.next = b
-                        b.next = d
-
-                        c.prev = a
-                        b.prev = c
-                        d.prev = b
-
+                        c.moveBack()
                         value++
                     } else {
-                        val b = c.prev
-                        val d = c.next
-                        val e = c.next.next
-
-                        b.next = d
-                        d.next = c
-                        c.next = e
-
-                        d.prev = b
-                        c.prev = d
-                        e.prev = c
-
+                        c.moveForward()
                         value--
                     }
                 }
-
             }
 
             val num1 = list.first { it.value == 0L }
             if (debug) debugPrintOrder(num1, repeatNum.toString())
         }
 
-
-        val num0 = list.first { it.value == 0L }
-
-        var loopis = num0.next
-        var counter = 1
-        val resultList = mutableListOf<Long>()
-
-        while (counter <= 3000) {
-            counter++
-            loopis = loopis.next
-            if (counter % 1000 == 0) {
-                resultList.add(loopis.value)
-            }
-        }
-
+        val resultList = getResult(list)
         return resultList.sum()
     }
 
